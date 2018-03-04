16b69d1982ba3748ec9923078637bebd0c3b86e3
Steven Schwenke <steven@stevenschwenke.de>
2018-03-04 07:27:40 +0100

    Add first CSS-styling

--- src/main/webapp/app/layouts/main/main.component.ts
+++ src/main/webapp/app/layouts/main/main.component.ts
@@ -5,7 +5,8 @@ import { Title } from '@angular/platform-browser';
 
 @Component({
     selector: 'jhi-main',
-    templateUrl: './main.component.html'
+    templateUrl: './main.component.html',
+    styleUrls: ['./main.component.css']
 })
 export class JhiMainComponent implements OnInit {
 
